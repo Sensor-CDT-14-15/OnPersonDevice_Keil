1a4c9707eb6233036b07b8d7b182b6f46c46009a
Josie Hughes <jaeh2@cam.ac.uk>
2015-07-07 19:21:43 +0100

    Keil Blinky - works.

---
+++ Blinky_LED/Flash/blinky.d
@@ -0,0 +1,7 @@
+.\flash\blinky.o: Blinky.c
+.\flash\blinky.o: C:\Keil_v5\ARM\Inc\Freescale\Kinetis\MKL25Z4.H
+.\flash\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm0plus.h
+.\flash\blinky.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\flash\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\flash\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\flash\blinky.o: C:\Keil_v5\ARM\Inc\Freescale\Kinetis\system_MKL25Z4.h
---
+++ Blinky_LED/Flash/system_mkl25z4.d
@@ -0,0 +1,7 @@
+.\flash\system_mkl25z4.o: system_MKL25Z4.c
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\Inc\Freescale\Kinetis\MKL25Z4.h
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm0plus.h
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\flash\system_mkl25z4.o: C:\Keil_v5\ARM\Inc\Freescale\Kinetis\system_MKL25Z4.h
